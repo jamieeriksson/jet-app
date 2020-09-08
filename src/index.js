9d8728c0b5ef49fdc69f0ee011669cce8845ec0b
Jamie Eriksson <jamieeriksson@gmail.com>
2020-09-08 15:34:23 -0700

    Initial nav bar, regular css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./tailwind.output.css";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import logo from "./logo.svg";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav className="nav-bar">
+        <img src={logo} className="nav-logo" alt="logo" />
+        <svg id="hamburger-menu" viewBox="0 0 100 80" width="40" height="40">
+          <rect width="100" height="20"></rect>
+          <rect y="30" width="100" height="20"></rect>
+          <rect y="60" width="100" height="20"></rect>
+        </svg>
+        <ul className="nav-menu">
+          <li className="nav-item" id="nav-link1">
+            <a href="/home" className="nav-link">
+              Home
+            </a>
+          </li>
+          <li className="nav-item" id="nav-link2">
+            <a href="/about" className="nav-link">
+              About
+            </a>
+          </li>
+          <li className="nav-item" id="nav-link3">
+            <a href="/services" className="nav-link">
+              Services
+            </a>
+          </li>
+          <li className="nav-item" id="nav-link4">
+            <a href="/contact" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Page extends React.Component {
+  render() {
+    return <NavBar />;
+  }
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
