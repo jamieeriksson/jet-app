5a52ed1941ebd5bc5afe278f813ba68d0dfa19bd
Jamie Eriksson <jamieeriksson@gmail.com>
2020-09-08 17:31:09 -0700

    Complete nav for mobile and desktop

--- src/index.js
+++ src/index.js
@@ -1,42 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./tailwind.output.css";
-import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import logo from "./logo.svg";
+import logo from "./assets/logo.svg";
 
-class NavBar extends React.Component {
+class NavItem extends React.Component {
   render() {
     return (
-      <nav className="nav-bar">
-        <img src={logo} className="nav-logo" alt="logo" />
-        <svg id="hamburger-menu" viewBox="0 0 100 80" width="40" height="40">
-          <rect width="100" height="20"></rect>
-          <rect y="30" width="100" height="20"></rect>
-          <rect y="60" width="100" height="20"></rect>
-        </svg>
-        <ul className="nav-menu">
-          <li className="nav-item" id="nav-link1">
-            <a href="/home" className="nav-link">
-              Home
-            </a>
-          </li>
-          <li className="nav-item" id="nav-link2">
-            <a href="/about" className="nav-link">
-              About
-            </a>
-          </li>
-          <li className="nav-item" id="nav-link3">
-            <a href="/services" className="nav-link">
-              Services
-            </a>
-          </li>
-          <li className="nav-item" id="nav-link4">
-            <a href="/contact" className="nav-link">
-              Contact
-            </a>
-          </li>
-        </ul>
+      <div
+        className={`${this.props.menuIsOpen ? "" : "hidden"} ${
+          this.props.bg
+        } flex align-center justify-center md:w-32 md:flex`}
+        id={`${this.props.id}`}
+      >
+        <a href={`/${this.props.link}`} className="p-3 md:p-4">
+          {this.props.linkName}
+        </a>
+      </div>
+    );
+  }
+}
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { menuIsOpen: false };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({ menuIsOpen: !this.state.menuIsOpen });
+  }
+
+  render() {
+    return (
+      <nav className="relative h-16 bg-gray-light text-xl md:flex md:justify-end">
+        <img
+          src={logo}
+          className="inline h-16 md:absolute md:left-0 md:ml-4"
+          alt="logo"
+        />
+        <button
+          className="inline absolute right-0 m-4 md:hidden"
+          onClick={this.toggleMenu}
+        >
+          <svg viewBox="0 0 100 80" width="40" height="40">
+            <rect width="100" height="8"></rect>
+            <rect y="30" width="100" height="8"></rect>
+            <rect y="60" width="100" height="8"></rect>
+          </svg>
+        </button>
+        <NavItem
+          id={"nav-link1"}
+          bg={"bg-gray"}
+          link={"home"}
+          linkName={"Home"}
+          menuIsOpen={this.state.menuIsOpen}
+        />
+        <NavItem
+          id={"nav-link2"}
+          bg={"bg-red"}
+          link={"about"}
+          linkName={"About"}
+          menuIsOpen={this.state.menuIsOpen}
+        />
+        <NavItem
+          id={"nav-link3"}
+          bg={"bg-yellow"}
+          link={"services"}
+          linkName={"Services"}
+          menuIsOpen={this.state.menuIsOpen}
+        />
+        <NavItem
+          id={"nav-link4"}
+          bg={"bg-teal-dark"}
+          link={"contact"}
+          linkName={"Contact"}
+          menuIsOpen={this.state.menuIsOpen}
+        />
       </nav>
     );
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,24 @@ module.exports = {
     "public/**/*.html",
   ],
   theme: {
-    extend: {},
+    colors: {
+      red: "#E76F51",
+      yellow: "#E9C46A",
+      teal: {
+        default: "#2A9D8F",
+        dark: "#264653",
+      },
+      gray: {
+        light: "#edf2f7",
+        default: "#e2e8f0",
+        dark: "#2d3748",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+      },
+    },
   },
   variants: {},
   plugins: [],
