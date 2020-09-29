09c359591de036f9c51faaeb8e6f54a56dfd48eb
Jamie Eriksson <jamieeriksson@gmail.com>
2020-09-28 21:27:11 -0700

    Add a scroll to top with navigation

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 import React from "react";
 import "./tailwind.output.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import NavBar from "./nav.js";
 import Home from "./home/home.js";
 import About from "./about/about.js";
 import Services from "./services/services.js";
 import Contact from "./contact/contact.js";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <NavBar />
       <Switch>
         <Route path="/" exact component={Home}></Route>
