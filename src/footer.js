efcf719c88a9b69ad7a8bf407e6d34b125dbe28e
Jamie Eriksson <jamieeriksson@gmail.com>
2020-10-13 07:29:56 -0700

    Add instagram social media back to footer

--- src/footer.js
+++ src/footer.js
@@ -2,40 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./tailwind.output.css";
 
-// import instagram from "./assets/icons8-instagram.svg";
+import instagram from "./assets/icons8-instagram.svg";
 // import twitter from "./assets/icons8-twitter.svg";
 // import facebook from "./assets/icons8-facebook.svg";
 
-// class Social extends React.Component {
-//   render() {
-//     return (
-//       <ul className="list-none font-light text-sm text-gray-600 p-3 md:p-10 md:pl-16">
-//         <li className="text-gray-500 font-normal text-sm uppercase mb-2">
-//           Social
-//         </li>
-//         <li className="mt-1 opacity-50 hover:opacity-100">
-//           <a href="https://www.instagram.com/jamieriksson/?hl=en">
-//             <img
-//               src={instagram}
-//               className="h-6 pl-1"
-//               alt="instagram logo"
-//             ></img>
-//           </a>
-//         </li>
-//         <li className="mt-1 opacity-50 hover:opacity-100">
-//           <a href="https://twitter.com/jamieriksson">
-//             <img src={twitter} className="h-6 pl-1" alt="twitter logo"></img>
-//           </a>
-//         </li>
-//         <li className="mt-1 opacity-50 hover:opacity-100">
-//           <a href="https://www.facebook.com/jamie.eriksson.5/">
-//             <img src={facebook} className="h-6 pl-1" alt="facebook logo"></img>
-//           </a>
-//         </li>
-//       </ul>
-//     );
-//   }
-// }
+class Social extends React.Component {
+  render() {
+    return (
+      <ul className="list-none font-light text-sm text-gray-600 mt-2">
+        <li className="text-gray-500 font-normal text-sm uppercase mb-2">
+          Social
+        </li>
+        <li className="mt-1 opacity-50 hover:opacity-100">
+          <a
+            href="https://www.instagram.com/jethrowing/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={instagram}
+              className="h-6 pl-1"
+              alt="instagram logo"
+            ></img>
+          </a>
+        </li>
+        {/* <li className="mt-1 opacity-50 hover:opacity-100">
+          <a href="https://twitter.com/jamieriksson" target="_blank" rel="noopener noreferrer">
+            <img src={twitter} className="h-6 pl-1" alt="twitter logo"></img>
+          </a>
+        </li>
+        <li className="mt-1 opacity-50 hover:opacity-100">
+          <a href="https://www.facebook.com/jamie.eriksson.5/" target="_blank" rel="noopener noreferrer">
+            <img src={facebook} className="h-6 pl-1" alt="facebook logo"></img>
+          </a>
+        </li> */}
+      </ul>
+    );
+  }
+}
 
 class Footer extends React.Component {
   render() {
@@ -67,6 +71,7 @@ class Footer extends React.Component {
               </Link>
             </li>
           </ul>
+
           <ul className="list-none font-light text-sm text-gray-600 p-3 md:p-10 md:pl-16">
             <li className="text-gray-500 font-normal text-sm uppercase mb-1">
               Contact
@@ -81,6 +86,7 @@ class Footer extends React.Component {
                 jamieeriksson@gmail.com
               </a>
             </li>
+            <Social />
           </ul>
         </div>
       </div>
