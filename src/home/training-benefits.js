53c749ce5418559bcde1fd3d9b1333dbb9bf9bbd
Jamie Eriksson <60158864+jamieeriksson@users.noreply.github.com>
2020-10-13 07:17:40 -0700

    Feature home styling (#6)

--- src/home/training-benefits.js
+++ src/home/training-benefits.js
@@ -20,7 +20,7 @@ function TrainingBenefits() {
           JET Training Benefits
         </h3>
         <div className="my-4 self-center grid gap-6 grid-cols-sm-1 md:grid-cols-sm-2 lg:grid-cols-sm-4 grid-flow-row font-sans text-3xl uppercase text-gray-dark">
-          <div className="relative h-56 w-56 shadow-lg bg-yellow">
+          <div className="relative h-56 w-56 shadow-lg bg-yellow transition duration-200 ease-in-out transform hover:scale-105">
             <p className="px-3 py-2 leading-9">Increased throwing distance</p>
             <FontAwesomeIcon
               icon={faRuler}
@@ -28,7 +28,7 @@ function TrainingBenefits() {
               className="absolute m-2 bottom-0 right-0"
             />
           </div>
-          <div className="relative h-56 w-56 shadow-lg bg-red">
+          <div className="relative h-56 w-56 shadow-lg bg-red transition duration-200 ease-in-out transform hover:scale-105">
             <FontAwesomeIcon
               icon={faEllipsisV}
               size="3x"
@@ -38,7 +38,7 @@ function TrainingBenefits() {
               More release points
             </p>
           </div>
-          <div className="relative h-56 w-56 shadow-lg bg-teal-bright">
+          <div className="relative h-56 w-56 shadow-lg bg-teal-bright transition duration-200 ease-in-out transform hover:scale-105">
             <p className="absolute right-0 px-3 py-2 text-right leading-9">
               Improved ability to throw in wind
             </p>
@@ -48,7 +48,7 @@ function TrainingBenefits() {
               className="absolute mb-1 ml-3 bottom-0 left-0"
             />
           </div>
-          <div className="relative h-56 w-56 shadow-lg bg-gray-medium">
+          <div className="relative h-56 w-56 shadow-lg bg-gray-medium transition duration-200 ease-in-out transform hover:scale-105">
             <p className="absolute bottom-0 px-3 py-2 leading-9">
               Improved throwing consistency
             </p>
@@ -58,7 +58,7 @@ function TrainingBenefits() {
               className="absolute mt-0 mr-5 top-0 right-0"
             />
           </div>
-          <div className="relative h-56 w-56 shadow-lg bg-yellow md:row-start-3 md:col-start-2 lg:row-start-2 lg:col-start-4">
+          <div className="relative h-56 w-56 shadow-lg bg-yellow md:row-start-3 md:col-start-2 lg:row-start-2 lg:col-start-4 transition duration-200 ease-in-out transform hover:scale-105">
             <p className="absolute bottom-0 right-0 px-4 py-3 text-right leading-9">
               Faster break throws
             </p>
@@ -68,7 +68,7 @@ function TrainingBenefits() {
               className="absolute mt-6 ml-8 top-0 left-0"
             />
           </div>
-          <div className="relative h-56 w-56 shadow-lg bg-red md:row-start-3 md:col-start-1 lg:row-start-2 lg:col-start-3">
+          <div className="relative h-56 w-56 shadow-lg bg-red md:row-start-3 md:col-start-1 lg:row-start-2 lg:col-start-3 transition duration-200 ease-in-out transform hover:scale-105">
             <p className="absolute right-0 px-4 py-3 text-right leading-9">
               Better angle control
             </p>
@@ -78,7 +78,7 @@ function TrainingBenefits() {
               className="absolute ml-1 -mb-6 bottom-0 left-0 transform rotate-45"
             />
           </div>
-          <div className="relative h-56 w-56 shadow-lg bg-teal-bright md:row-start-4 md:col-start-2 lg:row-start-2 lg:col-start-2">
+          <div className="relative h-56 w-56 shadow-lg bg-teal-bright md:row-start-4 md:col-start-2 lg:row-start-2 lg:col-start-2 transition duration-200 ease-in-out transform hover:scale-105">
             <p className="absolute bottom-0 left-0 px-3 py-1 leading-9">
               Wider range of throws
             </p>
@@ -88,7 +88,7 @@ function TrainingBenefits() {
               className="absolute m-4 top-0 right-0"
             />
           </div>
-          <div className="relative h-56 w-56 shadow-lg bg-gray-medium md:row-start-4 md:col-start-1 lg:row-start-2 lg:col-start-1">
+          <div className="relative h-56 w-56 shadow-lg bg-gray-medium md:row-start-4 md:col-start-1 lg:row-start-2 lg:col-start-1 transition duration-200 ease-in-out transform hover:scale-105">
             <p className="absolute top-0 right-0 px-3 py-1 text-right leading-9">
               Better control over disc speed
             </p>
@@ -99,7 +99,7 @@ function TrainingBenefits() {
             />
           </div>
         </div>
-        <h6 className="mt-16 self-end font-title text-gray-light opacity-75">
+        <h6 className="mt-16 mb-1 self-end font-title text-gray-light opacity-75">
           Interested in getting coached?
         </h6>
         <Link
