2f1e587d6faf7c0356c1f777f12495501f9f02f8
Jamie Eriksson <jamieeriksson@gmail.com>
2020-10-12 23:48:45 -0700

    Fix responsive layout on clinic redirect

--- src/services/clinic-redirect.js
+++ src/services/clinic-redirect.js
@@ -8,16 +8,16 @@ function ClinicRedirect() {
       <p className="mb-2 ml-2 font-title uppercase text-2xl">
         Searching for group services? Are you...
       </p>
-      <div className="p-4 flex justify-center place-items-center bg-gray-54 text-center text-lg font-title shadow-md">
+      <div className="p-4 flex flex-col md:flex-row justify-center place-items-center bg-gray-54 text-center text-lg font-title shadow-md">
         <p className="flex-1 p-6">
           A team looking to improve the throwing abilities of your players?
         </p>
-        <p className="flex-1 p-6 border-l-4 border-gray-dark">
+        <p className="flex-1 p-6 border-t-4 md:border-t-0 md:border-l-4 border-gray-dark">
           An organization looking to offer an opportunity for your players to
           grow and stay engaged with ultimate?
         </p>
       </div>
-      <p className="mt-6 mb-4 self-center font-light text-lg">
+      <p className="mt-6 mb-4 self-center font-light text-lg text-center">
         Check out our socially distanced throwing mechanics clinics
       </p>
       <Link
